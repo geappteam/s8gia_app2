46fac6d32252ebb6864361769ba71d57bf48d6ea
Gabriel Gibeau Sanchez <ggibeau@doubleg.info>
2019-09-22 22:24:05 -0400

    Start of Fuzzy Controller

--- problem_student/scripts/drive-fuzzy/main.py
+++ problem_student/scripts/drive-fuzzy/main.py
@@ -31,6 +31,8 @@ import os
 import sys
 import time
 import logging
+import numpy as np
+import skfuzzy as fuzzy
 
 sys.path.append('../..')
 from torcs.control.core import TorcsControlEnv, TorcsException, EpisodeRecorder
@@ -44,6 +46,226 @@ logger = logging.getLogger(__name__)
 # Define helper functions here
 ################################
 
+class FuzzyController(object):
+
+    def __init__(self, absEnabled=True):
+        self.absEnabled = absEnabled
+
+    # usage: GEAR = calculateGear(STATE)
+    #
+    # Calculate the gear of the transmission for the current car state.
+    # Adapted from the code of the WCCI2008 example C++ client:
+    # http://cig.ws.dei.polimi.it/wp-content/uploads/2008/04/client-cpp_v02.tgz
+    #
+    # Input:
+    # - STATE, a structure describing the current state of the car (see function 'waitForState').
+    #
+    # Output:
+    # - GEAR, the selected gear. -1 is reverse, 0 is neutral and the forward gear can range from 1 to 6.
+    #
+    def _calculateGear(self, state):
+
+        # Generer l'univers de discours (universe variables)
+        x_rpm = np.arange(0, 10001, 1)
+        x_speed = np.arange(0, 251, 1)
+        x_gear = np.arange(-1, 7, 1)
+
+        # Generer les fonctions d'appartenance (membership functions)
+        rpmL = fuzzy.trapmf(x_rpm, [0,0, 6500, 8000])
+        rpmM = fuzzy.trimf(x_rpm, [6500, 8000, 9500])
+        rpmH = fuzzy.trapmf(x_rpm, [800, 9500, 10000, 10000])
+
+        speedL = fuzzy.trimf(x_speed, [0., 0., 62.5])
+        speedML = fuzzy.trimf(x_speed, [0., 62.5, 125])
+        speedM = fuzzy.trimf(x_speed, [62.5, 125., 187.5])
+        speedMH = fuzzy.trimf(x_speed, [125., 187.5, 250])
+        speedH = fuzzy.trimf(x_speed, [187.5, 250, 250])
+
+        gearR = fuzzy.trimf(x_gear, [-1, -1, 0])
+        gearN = fuzzy.trimf(x_gear, [-1, 0, 1])
+        gear1 = fuzzy.trimf(x_gear, [0, 1, 2])
+        gear2 = fuzzy.trimf(x_gear, [1, 2, 3])
+        gear3 = fuzzy.trimf(x_gear, [2, 3, 4])
+        gear4 = fuzzy.trimf(x_gear, [3, 4, 5])
+        gear5 = fuzzy.trimf(x_gear, [4, 5, 6])
+        gear6 = fuzzy.trimf(x_gear, [5, 6, 6])
+
+
+        curGear = state['gear'][0]
+        curRpm = state['rpm'][0]
+        # Total speed is calculated using pythagoras
+        curSpeed = np.sqrt(np.sum(np.power(state['speed'], 2)))
+
+
+
+        return nextGear
+
+    # usage: STEERING = calculateSteering(STATE)
+    #
+    # Calculate the steering value for the current car state.
+    # Adapted from the code of the WCCI2008 example C++ client:
+    # http://cig.ws.dei.polimi.it/wp-content/uploads/2008/04/client-cpp_v02.tgz
+    #
+    # Input:
+    # - STATE, a structure describing the current state of the car (see function 'waitForState').
+    #
+    # Output:
+    # - STEERING, the steering value. -1 and +1 means respectively full left and right, that corresponds to an angle of 0.785398 rad.
+    #
+    def _calculateSteering(self, state):
+        # Steering constants
+        steerLock = 0.785398
+        steerSensitivityOffset = 80.0
+        wheelSensitivityCoeff = 1.0
+
+        curAngle = state['angle'][0]
+        curTrackPos = state['trackPos'][0]
+        curSpeedX = state['speed'][0]
+
+        # Steering angle is computed by correcting the actual car angle w.r.t. to track
+        # axis and to adjust car position w.r.t to middle of track
+        targetAngle = curAngle - curTrackPos * 2.0
+
+        # At high speed, reduce the steering command to avoid loosing control
+        if curSpeedX > steerSensitivityOffset:
+            steering = targetAngle / (steerLock * (curSpeedX - steerSensitivityOffset) * wheelSensitivityCoeff)
+        else:
+            steering = targetAngle / steerLock
+
+        # Normalize steering
+        steering = np.clip(steering, -1.0, 1.0)
+
+        return steering
+
+    # usage: ACCELERATION = calculateAcceleration(STATE)
+    #
+    # Calculate the accelerator (gas pedal) value for the current car state.
+    # Adapted from the code of the WCCI2008 example C++ client:
+    # http://cig.ws.dei.polimi.it/wp-content/uploads/2008/04/client-cpp_v02.tgz
+    #
+    # Input:
+    # - STATE, a structure describing the current state of the car (see function 'waitForState').
+    #
+    # Output:
+    # - ACCELERATION, the virtual gas pedal (0 means no gas, 1 full gas), in the range [0,1].
+    #
+    def _calculateAcceleration(self, state):
+
+        # Accel and Brake Constants
+        maxSpeedDist = 95.0
+        maxSpeed = 100.0
+        sin10 = 0.17365
+        cos10 = 0.98481
+        angleSensitivity = 2.0
+
+        curSpeedX = state['speed'][0]
+        curTrackPos = state['trackPos'][0]
+
+        # checks if car is out of track
+        if (curTrackPos < 1 and curTrackPos > -1):
+
+            # Reading of sensor at +10 degree w.r.t. car axis
+            rxSensor = state['track'][8]
+            # Reading of sensor parallel to car axis
+            cSensor = state['track'][9]
+            # Reading of sensor at -5 degree w.r.t. car axis
+            sxSensor = state['track'][10]
+
+            # Track is straight and enough far from a turn so goes to max speed
+            if cSensor > maxSpeedDist or (cSensor >= rxSensor and cSensor >= sxSensor):
+                targetSpeed = maxSpeed
+            else:
+                # Approaching a turn on right
+                if rxSensor > sxSensor:
+                    # Computing approximately the "angle" of turn
+                    h = cSensor * sin10
+                    b = rxSensor - cSensor * cos10
+                    angle = np.arcsin(b * b / (h * h + b * b))
+
+                # Approaching a turn on left
+                else:
+                    # Computing approximately the "angle" of turn
+                    h = cSensor * sin10
+                    b = sxSensor - cSensor * cos10
+                    angle = np.arcsin(b * b / (h * h + b * b))
+
+                # Estimate the target speed depending on turn and on how close it is
+                targetSpeed = maxSpeed * (cSensor * np.sin(angle) / maxSpeedDist) * angleSensitivity
+                targetSpeed = np.clip(targetSpeed, 0.0, maxSpeed)
+
+            # Accel/brake command is exponentially scaled w.r.t. the difference
+            # between target speed and current one
+            accel = (2.0 / (1.0 + np.exp(curSpeedX - targetSpeed)) - 1.0)
+
+        else:
+            # when out of track returns a moderate acceleration command
+            accel = 0.3
+
+        if accel > 0:
+            accel = accel
+            brake = 0.0
+        else:
+            brake = -accel
+            accel = 0.0
+
+            if self.absEnabled:
+                # apply ABS to brake
+                brake = self._filterABS(state, brake)
+
+        brake = np.clip(brake, 0.0, 1.0)
+        accel = np.clip(accel, 0.0, 1.0)
+
+        return accel, brake
+
+    def _filterABS(self, state, brake):
+
+        wheelRadius = [0.3179, 0.3179, 0.3276, 0.3276]
+        absSlip = 2.0
+        absRange = 3.0
+        absMinSpeed = 3.0
+
+        curSpeedX = state['speed'][0]
+
+        # convert speed to m/s
+        speed = curSpeedX / 3.6
+
+        # when speed lower than min speed for abs do nothing
+        if speed >= absMinSpeed:
+            # compute the speed of wheels in m/s
+            slip = np.dot(state['wheelSpinVel'], wheelRadius)
+            # slip is the difference between actual speed of car and average speed of wheels
+            slip = speed - slip / 4.0
+            # when slip too high apply ABS
+            if slip > absSlip:
+                brake = brake - (slip - absSlip) / absRange
+
+            # check brake is not negative, otherwise set it to zero
+            brake = np.clip(brake, 0.0, 1.0)
+
+        return brake
+
+    # usage: ACTION = drive(STATE)
+    #
+    # Calculate the accelerator, brake, gear and steering values based on the current car state.
+    #
+    # Input:
+    # - STATE, a structure describing the current state of the car (see function 'waitForState').
+    #
+    # Output:
+    # - ACTION, the structure describing the action to execute (see function 'applyAction').
+    #
+    def drive(self, state):
+        accel, brake = self._calculateAcceleration(state)
+        gear = self._calculateGear(state)
+        steer = self._calculateSteering(state)
+
+        action = {'accel': np.array([accel], dtype=np.float32),
+                  'brake': np.array([brake], dtype=np.float32),
+                  'gear': np.array([gear], dtype=np.int32),
+                  'steer': np.array([steer], dtype=np.float32)}
+        return action
+    
+    
 def main():
 
     recordingsPath = os.path.join(CDIR, 'recordings')
@@ -52,7 +274,8 @@ def main():
 
     try:
         with TorcsControlEnv(render=False) as env:
-
+            controller = FuzzyController()
+            
             nbTracks = len(TorcsControlEnv.availableTracks)
             nbSuccessfulEpisodes = 0
             for episode in range(nbTracks):
@@ -65,10 +288,18 @@ def main():
                 nbStepsShowStats = 1000
                 curNbSteps = 0
                 done = False
+                doIt = True
+                
                 with EpisodeRecorder(os.path.join(recordingsPath, 'track-%s.pklz' % (trackName))) as recorder:
                     while not done:
-                        # TODO: Select the next action based on the observation
-                        action = env.action_space.sample()
+                        # Select the next action based on the observation
+                        
+                        if(doIt):
+                            print("Observation: ", observation)
+                        action = controller.drive(observation)
+                        if(doIt):
+                            print("Action: ", action)
+                            doIt = False
                         recorder.save(observation, action)
     
                         # Execute the action
